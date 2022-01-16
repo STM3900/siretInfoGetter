329ca566104a873fb427c307e8564b3eee85d36f
Théo Migeat <44432694+STM3900@users.noreply.github.com>
2022-01-16 13:46:03 +0100

    debut requète api

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,39 @@
 <template>
-  <h1>siretInfoGetter</h1>
+  <div>
+    <h1>siretInfoGetter</h1>
+    <p>Salut {{ name }}</p>
+    <button @click="getCompanySiret">Recherche par SIRET</button>
+    <button @click="getCompanySiren">Recherche par SIREN</button>
+    <p>Données de la réponse API : {{ data }}</p>
+  </div>
 </template>
 
 <script>
 export default {
   name: "IndexPage",
+  data() {
+    return {
+      name: "Théo",
+      data: "",
+    };
+  },
+  methods: {
+    getCompanySiret() {
+      const res = this.$axios
+        .get(
+          "https://entreprise.data.gouv.fr/api/sirene/v1/siret/21310555400017"
+        )
+        .then((response) => {
+          this.data = response;
+        });
+    },
+    getCompanySiren() {
+      const res = this.$axios
+        .get("https://entreprise.data.gouv.fr/api/sirene/v1/siren/213105554")
+        .then((response) => {
+          this.data = response;
+        });
+    },
+  },
 };
 </script>
